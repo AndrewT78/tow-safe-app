02732a5800ce368cbeb87f3bbdbc6e129cd59cc3
Andrew Tyler <andrew.m.tyler@gmail.com>
2021-01-02 21:05:57 +0800

    Basic routing setup

--- src/App.js
+++ src/App.js
@@ -3,20 +3,37 @@ import './App.css';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import { Col, Row } from 'react-bootstrap';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
+    <Router>
     <div class="App">
-    <Container>      
-      <Row>
+    <Container> 
+      <Switch>
+        <Route path="/van">
+          <p>Configure the van</p>
+        </Route> 
+        <Route path="/">
+        <Row>
         <Col>
-        <Button variant="primary" size="lg" block>
-      Get Started
-    </Button>{' '}       
-    </Col>
-    </Row>
+          <Link to="van">
+            <Button variant="primary" size="lg" block>
+              Get Started
+           </Button> 
+         </Link>  
+        </Col>
+      </Row>
+        </Route> 
+      </Switch>          
     </Container>
     </div>
+    </Router>
   );
 }
 
