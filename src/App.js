851a4c58d0702f69e74531ef05d3bc0ded7a768e
Andrew Tyler <andrew.m.tyler@gmail.com>
2021-01-04 15:19:21 +0800

    Add some styling to the app

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
-import { Col, Row } from 'react-bootstrap';
+import { Col, Alert, Row } from 'react-bootstrap';
 import { connect } from "react-redux";
 
 import VanConfig from "./configcomponents/VanConfig"
@@ -11,13 +11,23 @@ import { getVanConfig } from "./redux/selectors";
 import { getCarConfig } from "./redux/selectors";
 
 
+
+const getVanConfigPane = () => {
+  return (<div><Alert variant="light">Welcome to TowSafe, Let's start by setting up your van details</Alert><VanConfig></VanConfig></div>);
+}
+
+const getCarConfigPane = () => {
+  return (<div><Alert variant="light">Now let's setup your tow vehicle</Alert><CarConfig></CarConfig></div>);
+}
+
 const App = ({ vanConfig, carConfig }) => {
+  
   return (
    
     <div>
     <Container>               
           {vanConfig && carConfig? <div>ATM : {vanConfig.atm}</div> : 
-            vanConfig ? <CarConfig></CarConfig> : <div>Welcome to TowSafe, Let's get setup shall we<VanConfig></VanConfig></div>
+            vanConfig ? getCarConfigPane() : getVanConfigPane()
           }
     </Container>
     </div>
