77259e1735b989fbcb1a016d228324742deb5093
Andrew Tyler <andrew.m.tyler@gmail.com>
2021-01-11 13:02:10 +0800

    Add enabled Status Icon

--- src/configcomponents/LoadItem.js
+++ src/configcomponents/LoadItem.js
@@ -1,26 +1,52 @@
-import React from 'react';
+import React from "react";
 import { Alert, Row, Col } from "react-bootstrap";
-import { FaTrash } from "react-icons/fa";
+import { FaToggleOn, FaToggleOff, FaTrash } from "react-icons/fa";
 
 class LoadItem extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {};
-      }
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-      render() {
-          return (
-              <Alert variant="secondary">
-                <Row>
-                  <Col>{this.props.item.item}</Col>                                    
-                </Row>
-                <Row>
-                  <Col>{this.props.item.quantity * this.props.item.weight} kgs ({this.props.item.quantity}x{this.props.item.weight} kg)</Col>
-                  <Col xs={'auto'}><FaTrash onClick={() => {this.props.handleDelete(this.props.item.id)}} size="25" data-testid={`delete-load-${this.props.item.id}`}></FaTrash></Col>
-                </Row>
-              </Alert>          
-          );
-      }
+  render() {
+    return (
+      <Alert variant="secondary">
+        <Row>
+          <Col>{this.props.item.item}</Col>
+        </Row>
+        <Row>
+          <Col>
+            {this.props.item.quantity * this.props.item.weight} kgs (
+            {this.props.item.quantity}x{this.props.item.weight} kg)
+          </Col>
+          <Col xs={"auto"}>
+            {this.props.item.enabled ? (
+              <FaToggleOn
+                onClick={() => {}}
+                size="25"
+                data-testid={`enabled-toggle-load-${this.props.item.id}`}
+              ></FaToggleOn>
+            ) : (
+              <FaToggleOff
+                onClick={() => {}}
+                size="25"
+                data-testid={`disabled-toggle-load-${this.props.item.id}`}
+              ></FaToggleOff>
+            )}
+          </Col>
+          <Col xs={"auto"}>
+            <FaTrash
+              onClick={() => {
+                this.props.handleDelete(this.props.item.id);
+              }}
+              size="25"
+              data-testid={`delete-load-${this.props.item.id}`}
+            ></FaTrash>
+          </Col>
+        </Row>
+      </Alert>
+    );
+  }
 }
 
 export default LoadItem;
--- src/configcomponents/LoadItem.test.js
+++ src/configcomponents/LoadItem.test.js
@@ -1,27 +1,47 @@
-import { render, screen, fireEvent } from '@testing-library/react';
-import LoadItem from './LoadItem';
+import { render, screen, fireEvent } from "@testing-library/react";
+import LoadItem from "./LoadItem";
 
+describe("Load Item", () => {
+  const load = {
+    item: "Engel",
+    quantity: 2,
+    weight: 25,
+    id: "Engel1",
+    enabled: true,
+  };
+  const disabledLoad = {
+    item: "Engel",
+    quantity: 2,
+    weight: 25,
+    id: "Engel1",
+    enabled: false,
+  };
 
-describe('Load Item', () => {
-
-  const load = { item: 'Engel', quantity: 2, weight: 25, id: "Engel1"};
-        
   const mockDelete = jest.fn();
 
-  it('renders the load item', () => {
-    render(<LoadItem item={load} handleDelete={mockDelete}/>);
+  it("renders the load item", () => {
+    render(<LoadItem item={load} handleDelete={mockDelete} />);
     screen.getByText(/Engel/);
     screen.getByText(/2/);
-    screen.getByText(/25/);        
+    screen.getByText(/25/);
   });
 
-  it('deletes the item when the delete button is pressed', () => {    
-    render(<LoadItem item={load} handleDelete={mockDelete}/>);
+  it("indicates the load item's enabled state", () => {
+    render(<LoadItem item={load} handleDelete={mockDelete} />);
+    const enabledToggle = screen.getByTestId("enabled-toggle-load-Engel1");
+  });
 
-    const deleteButton = screen.getByTestId('delete-load-Engel1');
-    fireEvent.click(deleteButton);   
+  it("indicates the load item's disabled state", () => {
+    render(<LoadItem item={disabledLoad} handleDelete={mockDelete} />);
+    const enabledToggle = screen.getByTestId("disabled-toggle-load-Engel1");
+  });
+
+  it("deletes the item when the delete button is pressed", () => {
+    render(<LoadItem item={load} handleDelete={mockDelete} />);
+
+    const deleteButton = screen.getByTestId("delete-load-Engel1");
+    fireEvent.click(deleteButton);
 
     expect(mockDelete).toHaveBeenCalledWith("Engel1");
-
   });
 });
