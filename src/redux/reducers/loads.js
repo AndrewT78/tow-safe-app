470e73d2e279ea421319630444ab9ee65af969d8
Andrew Tyler <andrew.m.tyler@gmail.com>
2021-01-11 12:47:47 +0800

    Enabling load items by default

--- src/redux/reducers/loads.js
+++ src/redux/reducers/loads.js
@@ -4,65 +4,60 @@ import { DELETE_CAR_LOAD } from "../actionTypes";
 import { DELETE_VAN_LOAD } from "../actionTypes";
 
 const initialState = {
-    carLoad: [],
-    vanLoad: []
+  carLoad: [],
+  vanLoad: [],
 };
 
 function guid() {
-    function _p8(s) {
-        var p = (Math.random().toString(16)+"000000000").substr(2,8);
-        return s ? "-" + p.substr(0,4) + "-" + p.substr(4,4) : p ;
-    }
-    return _p8() + _p8(true) + _p8(true) + _p8();
+  function _p8(s) {
+    var p = (Math.random().toString(16) + "000000000").substr(2, 8);
+    return s ? "-" + p.substr(0, 4) + "-" + p.substr(4, 4) : p;
+  }
+  return _p8() + _p8(true) + _p8(true) + _p8();
 }
 
-function removeLoadItem(loadArray, id) {       
-    
-    var index = loadArray.findIndex(l => l.id === id);
+function removeLoadItem(loadArray, id) {
+  var index = loadArray.findIndex((l) => l.id === id);
 
-    return [
-        ...loadArray.slice(0, index),
-        ...loadArray.slice(index+1, loadArray.length)
-    ];    
+  return [
+    ...loadArray.slice(0, index),
+    ...loadArray.slice(index + 1, loadArray.length),
+  ];
 }
 
-export default function(state = initialState, action) {
-        
-    switch (action.type) {
-        
-        case ADD_CAR_LOAD: {     
-            
-            action.load.id = guid();
-            
-            return {
-                ...state,
-                carLoad : [action.load].concat(state.carLoad)
-            };        
-        }        
-        case ADD_VAN_LOAD: {          
-            
-            action.load.id = guid();
-            
-            return {
-                ...state,
-                vanLoad : [action.load].concat(state.vanLoad)
-            };        
-        }        
-        case DELETE_CAR_LOAD: {   
-                        
-            return {
-                ...state,
-                carLoad: removeLoadItem(state.carLoad, action.id)
-            }
-        }
-        case DELETE_VAN_LOAD: {   
-           
-            return {
-                ...state,
-                vanLoad: removeLoadItem(state.vanLoad, action.id)
-            }
-        }
-        default:
-        return state;
+export default function (state = initialState, action) {
+  switch (action.type) {
+    case ADD_CAR_LOAD: {
+      action.load.id = guid();
+      action.load.enabled = true;
+
+      return {
+        ...state,
+        carLoad: [action.load].concat(state.carLoad),
+      };
     }
+    case ADD_VAN_LOAD: {
+      action.load.id = guid();
+      action.load.enabled = true;
+
+      return {
+        ...state,
+        vanLoad: [action.load].concat(state.vanLoad),
+      };
+    }
+    case DELETE_CAR_LOAD: {
+      return {
+        ...state,
+        carLoad: removeLoadItem(state.carLoad, action.id),
+      };
+    }
+    case DELETE_VAN_LOAD: {
+      return {
+        ...state,
+        vanLoad: removeLoadItem(state.vanLoad, action.id),
+      };
+    }
+    default:
+      return state;
+  }
 }
--- src/redux/reducers/loads.test.js
+++ src/redux/reducers/loads.test.js
@@ -1,92 +1,183 @@
-import reducer from './loads'
-import * as types from '../actionTypes'
+import reducer from "./loads";
+import * as types from "../actionTypes";
 
-describe('loads reducer', () => {
-  it('should return the initial state', () => {
-    expect(reducer(undefined, {})).toEqual(
-      {
-        carLoad: [],
-        vanLoad: []
-      }
-    )
-  })
- 
-  it('should add a load item to the car', () => {
-    expect(
-      reducer({carLoad: []}, {
-        type: types.ADD_CAR_LOAD,
-        load: { item: 'Weber', quantity: 1, weight: 20 }
-      }).carLoad
-    ).toEqual(expect.arrayContaining([expect.objectContaining({ item: 'Weber', quantity: 1, weight: 20 })]));
+describe("loads reducer", () => {
+  it("should return the initial state", () => {
+    expect(reducer(undefined, {})).toEqual({
+      carLoad: [],
+      vanLoad: [],
+    });
   });
 
-  it('should prepend a second load to the car', () => {
-
+  it("should add a load item to the car", () => {
     expect(
-      reducer({carLoad: [{item: "Engel", quantity: 1, weight: 25}]}, {
-        type: types.ADD_CAR_LOAD,
-        load: { item: 'Weber', quantity: 1, weight: 20 }
-      }).carLoad).toHaveLength(2);
-
-    expect(
-      reducer({carLoad: [{item: "Engel", quantity: 1, weight: 25}]}, {
-        type: types.ADD_CAR_LOAD,
-        load: { item: 'Weber', quantity: 1, weight: 20 }
-      }).carLoad[0]
-    ).toEqual(expect.objectContaining(
-      {
-        item: 'Weber', quantity: 1, weight: 20
-      }
-    )
-    )
-
+      reducer(
+        { carLoad: [] },
+        {
+          type: types.ADD_CAR_LOAD,
+          load: { item: "Weber", quantity: 1, weight: 20 },
+        }
+      ).carLoad
+    ).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({ item: "Weber", quantity: 1, weight: 20 }),
+      ])
+    );
   });
 
-  it('should delete a car load item', () => {
-     expect(
-      reducer({carLoad: [{item: "Engel", quantity: 1, weight: 25, id: 'Engel1'}, {item: "Beer", quantity: 24, weight: 1, id:"Beer1"}]}, {
-        type: types.DELETE_CAR_LOAD,
-        id: "Engel1"
+  it("should enable the car load item by default", () => {
+    expect(
+      reducer(
+        { carLoad: [] },
+        {
+          type: types.ADD_CAR_LOAD,
+          load: { item: "Weber", quantity: 1, weight: 20 },
+        }
+      ).carLoad
+    ).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          item: "Weber",
+          quantity: 1,
+          weight: 20,
+          enabled: true,
+        }),
+      ])
+    );
+  });
+
+  it("should prepend a second load to the car", () => {
+    expect(
+      reducer(
+        { carLoad: [{ item: "Engel", quantity: 1, weight: 25 }] },
+        {
+          type: types.ADD_CAR_LOAD,
+          load: { item: "Weber", quantity: 1, weight: 20 },
+        }
+      ).carLoad
+    ).toHaveLength(2);
+
+    expect(
+      reducer(
+        { carLoad: [{ item: "Engel", quantity: 1, weight: 25 }] },
+        {
+          type: types.ADD_CAR_LOAD,
+          load: { item: "Weber", quantity: 1, weight: 20 },
+        }
+      ).carLoad[0]
+    ).toEqual(
+      expect.objectContaining({
+        item: "Weber",
+        quantity: 1,
+        weight: 20,
       })
-    ).toEqual({carLoad: [{item: 'Beer', quantity: 24, weight: 1, id: "Beer1"}]});
-  })
-
-  it('should add a load item to the van', () => {
-    expect(
-      reducer({vanLoad: []}, {
-        type: types.ADD_VAN_LOAD,
-        load: { item: 'Pots & Pans', quantity: 5, weight: 2 }
-      }).vanLoad
-    ).toEqual(expect.arrayContaining([expect.objectContaining({ item: 'Pots & Pans', quantity: 5, weight: 2 })]));
-
+    );
   });
 
-  it('should prepend a second load to the van', () => {
+  it("should delete a car load item", () => {
     expect(
-      reducer({vanLoad: [{item: "Pots & Pans", quantity: 5, weight: 2}]}, {
-        type: types.ADD_VAN_LOAD,
-        load: { item: 'Sheets', quantity: 8, weight: 1 }
-      }).vanLoad).toHaveLength(2);
-
-    expect(
-      reducer({vanLoad: [{item: "Pots & Pans", quantity: 5, weight: 2}]}, {
-        type: types.ADD_VAN_LOAD,
-        load: { item: 'Sheets', quantity: 8, weight: 1 }
-      }).vanLoad[0]
-    ).toEqual(expect.objectContaining(
-      {
-        item: 'Sheets', quantity: 8, weight: 1
-      }
-    )
-    )
+      reducer(
+        {
+          carLoad: [
+            { item: "Engel", quantity: 1, weight: 25, id: "Engel1" },
+            { item: "Beer", quantity: 24, weight: 1, id: "Beer1" },
+          ],
+        },
+        {
+          type: types.DELETE_CAR_LOAD,
+          id: "Engel1",
+        }
+      )
+    ).toEqual({
+      carLoad: [{ item: "Beer", quantity: 24, weight: 1, id: "Beer1" }],
+    });
   });
 
-  it('should delete a van load item', () => {
+  it("should add a load item to the van", () => {
     expect(
-     reducer({vanLoad: [{item: "Sheets", quantity: 8, weight: 1, id: 'Sheets1'}, {item: "Food", quantity: 40, weight: 1, id:"Food1"}]}, {
-       type: types.DELETE_VAN_LOAD,
-       id: "Food1"
-     })
-   ).toEqual({vanLoad: [{item: 'Sheets', quantity: 8, weight: 1, id: "Sheets1"}]});
- })
+      reducer(
+        { vanLoad: [] },
+        {
+          type: types.ADD_VAN_LOAD,
+          load: { item: "Pots & Pans", quantity: 5, weight: 2 },
+        }
+      ).vanLoad
+    ).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          item: "Pots & Pans",
+          quantity: 5,
+          weight: 2,
+        }),
+      ])
+    );
+  });
+
+  it("should enable the van load item by default", () => {
+    expect(
+      reducer(
+        { vanLoad: [] },
+        {
+          type: types.ADD_VAN_LOAD,
+          load: { item: "Weber", quantity: 1, weight: 20 },
+        }
+      ).vanLoad
+    ).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          item: "Weber",
+          quantity: 1,
+          weight: 20,
+          enabled: true,
+        }),
+      ])
+    );
+  });
+
+  it("should prepend a second load to the van", () => {
+    expect(
+      reducer(
+        { vanLoad: [{ item: "Pots & Pans", quantity: 5, weight: 2 }] },
+        {
+          type: types.ADD_VAN_LOAD,
+          load: { item: "Sheets", quantity: 8, weight: 1 },
+        }
+      ).vanLoad
+    ).toHaveLength(2);
+
+    expect(
+      reducer(
+        { vanLoad: [{ item: "Pots & Pans", quantity: 5, weight: 2 }] },
+        {
+          type: types.ADD_VAN_LOAD,
+          load: { item: "Sheets", quantity: 8, weight: 1 },
+        }
+      ).vanLoad[0]
+    ).toEqual(
+      expect.objectContaining({
+        item: "Sheets",
+        quantity: 8,
+        weight: 1,
+      })
+    );
+  });
+
+  it("should delete a van load item", () => {
+    expect(
+      reducer(
+        {
+          vanLoad: [
+            { item: "Sheets", quantity: 8, weight: 1, id: "Sheets1" },
+            { item: "Food", quantity: 40, weight: 1, id: "Food1" },
+          ],
+        },
+        {
+          type: types.DELETE_VAN_LOAD,
+          id: "Food1",
+        }
+      )
+    ).toEqual({
+      vanLoad: [{ item: "Sheets", quantity: 8, weight: 1, id: "Sheets1" }],
+    });
+  });
 });
