8b738ecfc7f6e693448d4c00c8602bef1e5e6b84
Andrew Tyler <andrew.m.tyler@gmail.com>
2021-01-19 08:29:58 +0800

    Adding missing reducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,5 +1,6 @@
 import { combineReducers } from "redux";
 import configs from "./configs";
 import loads from "./loads";
+import accessories from "./accessories";
 
-export default combineReducers({ configs, loads });
+export default combineReducers({ configs, loads, accessories });
