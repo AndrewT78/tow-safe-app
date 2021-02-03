2ae48795ac2f513ca0129bbfeca22f4305f99253
Andrew Tyler <andrew.m.tyler@gmail.com>
2021-02-03 08:41:14 +0800

    Remove unused code

--- src/configcomponents/accessories/AccessoriesAdder.js
+++ src/configcomponents/accessories/AccessoriesAdder.js
@@ -4,11 +4,6 @@ import AddAccessory from "./AddAccessory";
 import AccessoryToggle from "./AccessoryToggle";
 
 class AccessoriesAdder extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { index: 0, weight: props.accessories[0].weight, hidden: [] };
-  }
-
   handleAddNewAccessory = (acc) => {
     this.props.handleAdd(acc);
   };
@@ -17,32 +12,18 @@ class AccessoriesAdder extends React.Component {
     this.props.handleDelete(id);
   };
 
-  markAsAdded = (key) => {
-    var newHidden = this.state.hidden;
-    newHidden.push(key);
-    this.setState({ hidden: newHidden });
-  };
-
-  markAsRemoved = (key) => {
-    var newHidden = this.state.hidden;
-    newHidden.pop(key);
-    this.setState({ hidden: newHidden });
-  };
-
   getSampleAccessories() {
     return this.props.accessories.map((prop, key) => {
-      if (this.state.hidden.indexOf(key) < 0 && !prop.exists) {
+      if (!prop.exists) {
         return (
           <div style={{ marginBottom: "20px" }} key={prop.accessory}>
             <AccessoryToggle
               accessory={prop}
               handleAddAccessory={(acc) => {
                 this.handleAddNewAccessory(acc);
-                this.markAsAdded(key);
               }}
               handleDeleteAccessory={(id) => {
                 this.handleDeleteAccessory(id);
-                this.markAsRemoved(key);
               }}
               toggle={false}
             ></AccessoryToggle>
@@ -55,11 +36,9 @@ class AccessoriesAdder extends React.Component {
               accessory={prop}
               handleAddAccessory={(acc) => {
                 this.handleAddNewAccessory(acc);
-                this.markAsAdded(key);
               }}
               handleDeleteAccessory={(id) => {
                 this.handleDeleteAccessory(id);
-                this.markAsRemoved(key);
               }}
               toggle={true}
             ></AccessoryToggle>
